74ee7dd7e4b263e0624e4ac575737ed8332c42e0
Karen Rustad <karen.rustad@gmail.com>
2013-10-29 00:02:20 -0700

    stuff

--- app.py
+++ app.py
@@ -8,17 +8,19 @@ DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/karen/Code/elsinore/test.db'
-db = SQLAlchemy(app)
 
 try:
     session_secret = os.environ['SESSION_SECRET']
+    postgres_password = os.environ['DB_PWD']
 except KeyError:
-    from localconfig import secret
+    from localconfig import secret, postgres_password
+    postgres_password = postgres_password
     session_secret = secret
 
 app.secret_key = session_secret
 csrf(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://annie:' + postgres_password + '@localhost/annotationdb'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -31,16 +33,11 @@ def about():
 @app.route('/api/annotation', methods=['GET', 'POST'])
 def api():
     if request.method == 'POST':
-        print request.form
+        # print request.form
         a = Annotation(request.form['line'], request.form['author'], request.form['text'])
-        print a
-        try:
-            db.session.add(a)
-            db.session.commit()
-        except:
-            db.create_all()
-            db.session.add(a)
-            db.session.commit()
+        # print a
+        db.session.add(a)
+        db.session.commit()
         dico = {
             'id': a.id, 
             'author': a.author,
@@ -49,11 +46,7 @@ def api():
             }
         return json.dumps(dico)
     else:
-        try:
-            all_annotations = Annotation.query.all()
-        except:
-            db.create_all()
-            all_annotations = Annotation.query.all()
+        all_annotations = Annotation.query.all()
         data = {}
         data['keys'] = []
         for annotation in all_annotations:
